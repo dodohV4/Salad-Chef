b209b9b375515c74d9b85586bcce87dfd56494fc
Hebin <45360456+dodohV4@users.noreply.github.com>
2019-04-08 02:41:12 +0530

    Add files via upload
    Basic Movement of player is done. Selecting of vegetables also done

---
+++ Assets/Scripts/PlayerInteractionController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteractionController : MonoBehaviour
+{
+
+    #region Static Variables
+
+    #endregion
+
+    #region Public Variables
+	public PlayerActions actionRef;
+    public KeyCode rightButton;
+    public KeyCode leftButton;
+    public KeyCode forwardButton;
+    public KeyCode backButton;
+    public KeyCode selectButton;
+    #endregion
+
+    #region Private Variables
+    private Rigidbody _rbPlayer;
+    [SerializeField]
+    private float _playerSpeed;
+    #endregion
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(rightButton))
+        {
+            transform.Translate(Vector3.right * _playerSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(leftButton))
+        {
+            transform.Translate(Vector3.left * _playerSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(forwardButton))
+        {
+            transform.Translate(Vector3.forward * _playerSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(backButton))
+        {
+            transform.Translate(Vector3.back * _playerSpeed * Time.deltaTime);
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (Input.GetKeyDown(selectButton))
+        {
+            if (other.tag == "Vegetable")
+            {
+				actionRef.AddVegetable(other.gameObject.GetComponent<Vegetables>().vegId);
+            }
+        }
+    }
+}
