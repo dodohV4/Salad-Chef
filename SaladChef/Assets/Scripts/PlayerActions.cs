254663f0f4a2897c360b931ad1101847f6799f62
Hebin Aloor <hebinhv4@gmail.com>
2019-04-08 02:55:10 +0530

    Test

---
+++ SaladChef/Assets/Scripts/PlayerActions.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerActions : MonoBehaviour
+{
+    public static int vegCount = 0;
+
+    public List<int> selectedVegetables;
+
+    public void AddVegetable(int veg)
+    {
+        Debug.LogFormat("Veg selected ::: {0}, Player ::: {1}", veg, this.gameObject.name);
+
+        if (vegCount < 2)
+        {
+            selectedVegetables[vegCount] = veg;
+            vegCount++;
+        }
+
+        else
+        {
+            if (vegCount >= 2)
+            {
+                vegCount = 0;
+            }
+            selectedVegetables[vegCount] = veg;
+            vegCount++;
+        }
+    }
+}
